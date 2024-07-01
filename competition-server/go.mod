3405763659628d3d3c9fd57797302fe2cdd57958
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-06-30 23:41:59 -0700

    Start competition-server directory

---
+++ competition-server/go.mod
@@ -0,0 +1,3 @@
+module competition_server
+
+go 1.22.0
---
+++ competition-server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const PORT = ":8000"
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	body := "Thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", body)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	mux.Handle("/", http.HandlerFunc(defaultHandler))
+	fmt.Println("Ready to serve at", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
